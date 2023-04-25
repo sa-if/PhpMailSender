6fc87bd5ea708d03f521e5bfbb2c6c6ff0eba128
Saif Islam <saifislam23122005@gmail.com>
2023-04-25 12:16:19 +0600

    Update script.php

--- script.php
+++ script.php
@@ -1,24 +1,32 @@
 <?php
+$to = "recipient@example.com";
+$subject = "Test Email";
+$message = "This is a test email sent from a PHP script.";
+$headers = "From: sender@example.com\r\n";
+$headers .= "Reply-To: sender@example.com\r\n";
+$headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-// Replace with your own email address and password
-$email = 'your-email@gmail.com';
-$password = 'your-password';
+// Add file attachments
+$file_attachments = array("file1.txt", "file2.pdf");
+foreach ($file_attachments as $file) {
+    $file_contents = file_get_contents($file);
+    $file_encoded = chunk_split(base64_encode($file_contents));
+    $headers .= "Content-Type: application/octet-stream; name=\"" . basename($file) . "\"\r\n";
+    $headers .= "Content-Disposition: attachment; filename=\"" . basename($file) . "\"\r\n";
+    $headers .= "Content-Transfer-Encoding: base64\r\n\r\n";
+    $headers .= $file_encoded . "\r\n";
+}
 
-// Replace with the recipient's email address
-$to = 'recipient-email@gmail.com';
+// Send HTML email
+$html_message = "<html><body><h1>This is a test email sent from a PHP script.</h1></body></html>";
+$html_headers = "From: sender@example.com\r\n";
+$html_headers .= "Reply-To: sender@example.com\r\n";
+$html_headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
-// Replace with a custom subject
-$subject = 'Test email from PHP';
-
-// Replace with a custom message
-$message = 'This is a test email sent from PHP';
-
-// Set up the mail headers
-$headers = "From: $email\r\n" .
-           "Reply-To: $email\r\n" .
-           "MIME-Version: 1.0\r\n" .
-           "Content-Type: text/html; charset=iso-8859-1\r\n";
-
-// Send the email
-mail($to, $subject, $message, $headers);
-
+// Send email and handle errors
+if (mail($to, $subject, $message, $headers) && mail($to, $subject, $html_message, $html_headers)) {
+    echo "Email sent successfully!";
+} else {
+    echo "Email sending failed.";
+}
+?>
